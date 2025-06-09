68bb29dbf39ca724fde51493a4616657b3de3441
isolhsolfafa <angdredong@gmail.com>
2025-06-09 21:33:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -112,7 +112,6 @@ const FactoryDashboard = () => {
   return (
     <div>
       <div className="header">
-        <img src="https://rainbow-haupia-cd8290.netlify.app/GST_banner.jpg" alt="Build up GST Banner" />
         <h1>제조기술1팀 공장 대시보드 - {getCurrentWeek()}</h1>
         <p>실행 시간: {currentTime}</p>
       </div>
