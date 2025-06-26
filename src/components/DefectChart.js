36c5492a1184523bd39fbcd50b168d762ca8beb4
isolhsolfafa <angdredong@gmail.com>
2025-06-26 22:48:48 +0900

    Update DefectChart.js

--- src/components/DefectChart.js
+++ src/components/DefectChart.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const DefectChart = () => (
   <div className="chart-section">
-    <h2>🔍 모델별불량예상[ML기반]-TEST</h2>
     <iframe
       src="/pie_defect.html"
       style={{ width: '100%', height: '250px', border: 'none' }}
