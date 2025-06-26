23e223fd211c75ba00bb958018fff6125d266e2c
isolhsolfafa <angdredong@gmail.com>
2025-06-26 23:49:29 +0900

    Update DefectChart.js

--- src/components/DefectChart.js
+++ src/components/DefectChart.js
@@ -4,7 +4,7 @@ const DefectChart = () => (
   <div className="chart-section defect-chart">
     <iframe
       src="/pie_defect.html"
-      style={{ width: '100%', height: '250px', border: 'none' }}
+      style={{ width: '100%', height: '270px', border: 'none' }}
       onError={(e) => e.target.parentNode.innerHTML = '<p>데이터 없음</p>'}
     />
   </div>
